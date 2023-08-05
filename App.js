e74761b3c5aa2d293ecf30f5abf544ce4959e24a
R.H.Mohamed Riffath <107987180+riffathRHM@users.noreply.github.com>
2023-08-05 12:40:22 +0530

    Add files via upload
    Frontend

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './layout/NavBar';
+import Home from './pages/Home';
+import {  BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
+import ViewUser from './users/ViewUser';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path ="/" element={<Home/>}></Route>
+        <Route exact path ="/adduser" element={<AddUser/>}></Route>
+        <Route exact path="/edituser/:id" element={<EditUser/>}> </Route>
+        <Route exact path="/viewuser/:id" element={<ViewUser/>}> </Route>
+      </Routes>
+      
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
