e4769fea6ade8f4c6e83dcdc4b65a4c7511becf9
R.H.Mohamed Riffath <107987180+riffathRHM@users.noreply.github.com>
2023-08-05 12:37:42 +0530

    Add files via upload
    Backend SpringBoot

---
+++ fullstack-backend/src/main/java/com/crud/fullstackbackend/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.crud.fullstackbackend.controller;
+
+import com.crud.fullstackbackend.exception.UserNotFoundException;
+import com.crud.fullstackbackend.model.User;
+import com.crud.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser (@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user->{
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setName(newUser.getName());
+                    return userRepository.save(user);
+                }).orElseThrow(
+                        ()->new UserNotFoundException(id)
+                );
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id)
+    {
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return  "User with id"+id+"has been deleted success";
+    }
+
+
+
+
+
+}
